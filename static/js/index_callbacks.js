838ffec0faeb9cf7059da01d136722dbcaf79115
Josnei Luis Olszewski Junior <josneijr@gmail.com>
2018-01-06 18:00:28 -0200

    Callbacks adicionadas aos botões

---
+++ static/js/index_callbacks.js
@@ -0,0 +1,7 @@
+$('#markers').click(function(){
+    alert('1');
+});
+
+$('#heatMap').click(function(){
+    alert('2');
+});
--- static/js/index_map.js
+++ static/js/index_map.js
@@ -3,12 +3,21 @@ var map = null;
 function AdicionaPontosTeste()
 {
       var heatMapData = [
-        {location: new google.maps.LatLng(-25.45, -49.26), weight: 1}
+        {location: new google.maps.LatLng(-25.451, -49.26), weight: 60},
+        {location: new google.maps.LatLng(-25.452, -49.26), weight: 80},
+        {location: new google.maps.LatLng(-25.453, -49.26), weight: 100},
+        {location: new google.maps.LatLng(-25.454, -49.26), weight: 80}
       ];
 
+      var gradientColors = [
+        'rgba(0, 255, 0, 0)',
+        'rgba(255, 0, 0, 1)'
+    ]
+
       var heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatmapData,
-        map: map
+        data: heatMapData,
+        map: map,
+        radius: 40
       });
 }
 
